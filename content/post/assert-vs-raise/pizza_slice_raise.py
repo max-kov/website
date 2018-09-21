4f8dadc208f9390ab780799e990ee02b5bc8f742
Max Kovalovs <maxkovalovs@gmail.com>
2018-09-21 23:47:11 +0200

    Fixed python errors in assert-vs-raise post
    Also added `force` flag in `setup.R`

---
+++ content/post/assert-vs-raise/pizza_slice_raise.py
@@ -0,0 +1,12 @@
+def calculate_pizza_area(pizza_radius:int, people_eating=1):
+    '''
+    Calculates the area of the pizza that each person will receive, assuming
+    the pizza is split equally among all people.
+    '''
+    if people_eating==1:
+        raise ZeroDivisionError
+    return (3.1415*(pizza_radius**2))/people_eating
+
+
+print(calculate_pizza_area(3))
+print(calculate_pizza_area(3, people_eating=0))
--- setup.R
+++ setup.R
@@ -1,4 +1,4 @@
 install.packages("blogdown")
 install.packages("reticulate")
 blogdown::install_hugo()
-blogdown::install_theme("road2stat/hugo-tanka")
+blogdown::install_theme("road2stat/hugo-tanka", force=TRUE)
