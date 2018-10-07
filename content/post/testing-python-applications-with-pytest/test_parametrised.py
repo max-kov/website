f5bf18ff76ed0b82da9dfc733bd3c28b8f9a5734
Max Kovalovs <maxkovalovs@gmail.com>
2018-10-07 11:27:30 +0100

    Edited pytest post

--- content/post/testing-python-applications-with-pytest/test_parametrised.py
+++ content/post/testing-python-applications-with-pytest/test_parametrised.py
@@ -1,10 +1,15 @@
 import pytest
 
-class TestMath:
-    @pytest.mark.parametrize("test_input, expected", [
-        ("3+5", 8),
-        ("2+4", 6),
-        ("6*9", 42),
-    ])
-    def test_eval(self, test_input, expected):
-        assert eval(test_input) == expected
+class TestPower:
+    @pytest.mark.parametrize("testing_input, expected",
+                             [
+                                 ((2,3),8),
+                                 ((2,2),4),
+                                 ((2,1),2)
+                                   ],ids=[
+                            "Two to the power of three",
+                            "Two to the power of two",
+                            "Two to the power of one"
+                             ])
+    def test_power(testing_input, expected):
+        assert testing_input[0]**testing_input[1]==expected
